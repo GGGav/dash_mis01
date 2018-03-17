773a22bbcad30c2ff9a1a7cadd0cf8d30ed0bb0a
GGGav <gggav@gggav.com>
2018-03-17 11:50:02 +0000

    Now works with HTTPS

--- jobs/users.rb
+++ jobs/users.rb
@@ -8,19 +8,36 @@
 #  Could use this as https template
 #         https://gist.github.com/pszypowicz/bc07528ed7ae79bf49aa
 
-require 'net/http'
+#require 'net/http'
+require 'net/https'
 require 'json'
+
+cert = File.read("./certs/aspire.crt")
+key = File.read("./certs/aspire-nopw.key")
+
+host = 'aspire.local'
+port = '3010'
+http = Net::HTTP.new(host, port)
+http.use_ssl = true
+http.cert = OpenSSL::X509::Certificate.new(cert)
+http.key = OpenSSL::PKey::RSA.new(key)
+http.ca_file = './certs/CA1.crt'
+#Not secure is to leave it NONE, VERIFY_PEER is preffered.
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+terminals_uri = '/monitor/mis/users'
 
 data = { registered: 0, pending: 0}
 
 SCHEDULER.every '5s', :first_in => 0 do |job|
 
-  http = Net::HTTP.new('192.168.0.240', '3000')
-  resp = http.request(Net::HTTP::Get.new("/monitor/mis/users"))
-  next unless '200'.eql? resp.code
+  # http = Net::HTTP.new('192.168.0.240', '3000')
+  # resp = http.request(Net::HTTP::Get.new("/monitor/mis/users"))
+  # next unless '200'.eql? resp.code
 
   dataprev = data
-
+  resp = http.get(terminals_uri)
+  next unless '200'.eql? resp.code
   data = JSON.parse(resp.body)
 
   if data["registered"] != dataprev["registered"]
